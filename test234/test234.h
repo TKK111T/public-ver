65df85a2ee83e6595ab997beb9e24c9dc7a75465
TKK111T <shankouy88@gmail.com>
2024-10-13 16:50:46 +0900

    yu

---
+++ test234/test234.h
@@ -0,0 +1,32 @@
+﻿
+// test234.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// Ctest234App:
+// このクラスの実装については、test234.cpp を参照してください
+//
+
+class Ctest234App : public CWinApp
+{
+public:
+	Ctest234App();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Ctest234App theApp;
